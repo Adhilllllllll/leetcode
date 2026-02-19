e42c590ac255963b5ae5250689fe2271e57d2e37
Adhil Padinjakara <adhilp387@gmail.com>
2026-02-19 17:44:30 +0530

    Time: 0 ms (100%), Space: 58 MB (35.03%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -1,9 +1,10 @@
- 
-var singleNumber = function(nums) {
-    let result=0;
 
-    for(let num of nums){
+
+var singleNumber = function (nums) {
+    let result = 0;
+
+    for (let num of nums) {
         result ^= num;
     }
-    return result ;
+    return result;
 };
